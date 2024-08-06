43e1e2c5252adb24521f37242698d5738213fc42
Bogdan Ungureanu <ungureanu.bogdan.sorin@gmail.com>
2024-08-06 08:14:02 +0300

    add and setup devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,40 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 20_240_806_044_258) do # rubocop:disable Metrics/BlockLength
+ActiveRecord::Schema[7.2].define(version: 2024_08_06_045238) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'products', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email'
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table 'wishlist_items', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.bigint 'wishlist_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_wishlist_items_on_user_id'
-    t.index ['wishlist_id'], name: 'index_wishlist_items_on_wishlist_id'
+  create_table "wishlist_items", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "wishlist_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_wishlist_items_on_user_id"
+    t.index ["wishlist_id"], name: "index_wishlist_items_on_wishlist_id"
   end
 
-  create_table 'wishlists', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_wishlists_on_user_id'
+  create_table "wishlists", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_wishlists_on_user_id"
   end
 
-  add_foreign_key 'wishlist_items', 'users'
-  add_foreign_key 'wishlist_items', 'wishlists'
-  add_foreign_key 'wishlists', 'users'
+  add_foreign_key "wishlist_items", "users"
+  add_foreign_key "wishlist_items", "wishlists"
+  add_foreign_key "wishlists", "users"
 end
